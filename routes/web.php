ba024bbb0684efb00e387113dbdf50dc2337924b
RoyKomodo <hasby_shippunden@yahoo.co.id>
2018-05-14 23:50:34 +0700

    superadmin
    tambahan

--- routes/web.php
+++ routes/web.php
@@ -71,6 +71,6 @@ Route::group(['prefix' => 'member'], function()
 	});
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+// Auth::routes();
+//
+// Route::get('/home', 'HomeController@i ndex')->name('home');
