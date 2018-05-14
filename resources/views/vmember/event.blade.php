f0cd19bb4951d719197dffcb4e6c0a23b9e1d7b1
pandhup <pandhuprisnawan@gmail.com>
2018-05-15 02:26:11 +0700

    nambah dikit

--- resources/views/vmember/event.blade.php
+++ resources/views/vmember/event.blade.php
@@ -69,7 +69,6 @@
                               <label class="control-label col-md-3 col-sm-3 col-xs-12">File Poster</label>
                               <div class="col-md-9 col-sm-9 col-xs-12">
                                 <div class="x_content">
-                                  <p>Drag multiple files to the box below for multi upload or click to select files. This is for demonstration purposes only, the files are not uploaded to any server.</p>
                                   <form action="form_upload.html" class="dropzone"></form>
                                   <br />
                                 </div>
